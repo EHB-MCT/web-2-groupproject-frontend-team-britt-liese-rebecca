33df5547c709be7d52fb774f7af1c648dabe8ad0
BrittHellinckx <britt.hellinckx@student.ehb.be>
2021-11-30 16:48:06 +0100

    starting listblok
    design how the listblock would look+
    adding starting code

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
 "use strict"
 
+window.onload = function () {
+    displayAll();
+
+    document.getElementById('form').addEventListener('submit', e => {
+        e.preventDefault();
+        newChallenge();
+    })
+}
+
+function displayAll() {
+    fetch(`url`)
+        .then(response => response.json())
+        .then(function (data) {
+            let challenge = document.getElementById('challengeBlock')
+            let htmlString = ""
+
+            data.forEach(challenge => {
+                htmlString +=
+                    `<article id="card">
+                        <div>
+                            <h3>${challenge.name}</h3>
+                        </div>
+                        <div id="info">
+                            <p>${challenge.course}</p>
+                            <p>${challenge.points}</p>
+                            <p>${challenge.session}</p>
+                        </div>
+                    </article>`
+            })
+            challenge.innerHTML = htmlString
+        });
+}
+
+function newChallenge() {
+    fetch(`url`)
+        .then(response => response.json())
+        .then(function (data) {
+            console.log(data)
+        })
+    //add new challenge to all challenges
+    displayAll();
+}
